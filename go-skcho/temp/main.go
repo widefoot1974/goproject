c1e2ec5c9fb25cb107723d9c6aa88c03aff01fa7
widefoot1974 <widefoot1974@naver.com>
2024-03-10 06:51:50 +0900

    add go-skcho/temp

---
+++ go-skcho/temp/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Printf("")
+
+	// ages := make(map[string]int)
+	// ages["John"] = 37
+	// ages["Mary"] = 24
+	// ages["Mary"] = 21 //overwrites 24
+
+	ages = map[srting]int{}
+
+}
