5f91aad1507e2dc5cb6e27c07a11945e279d943b
widefoot1974 <widefoot1974@naver.com>
2024-05-06 22:35:05 +0900

    .

--- go-skcho/xmlReadWrite/main.go
+++ go-skcho/xmlReadWrite/main.go
@@ -29,12 +29,32 @@ func main() {
 	}
 	defer file.Close()
 
+	// 구조체를 XML로 변환
 	encoder := xml.NewEncoder(file)
 	encoder.Indent("", "  ")
 	err = encoder.Encode(person)
 	if err != nil {
 		log.Panic(err)
 	}
+
+	xmlData, err := xml.MarshalIndent(person, "", "  ")
+	// xmlData, err := xml.Marshal(person)
+	if err != nil {
+		log.Panicf("[ERROR] xml.MarshalIndent() Fail: %v\n", err)
+		return
+	}
+	log.Printf("xmlData Type is %T\n", xmlData)
+	log.Printf("xmlData size is %v", len(xmlData))
+	log.Printf("xmlData =\n%v\n", string(xmlData))
+
+	xmlInput := string(xmlData)
+	var p2 Person
+	err = xml.Unmarshal([]byte(xmlInput), &p2)
+	if err != nil {
+		log.Panicf("[ERROR] xml.Unmarshal() Fail: %v\n", err)
+		return
+	}
+	fmt.Printf("p2 = %+v\n", p2)
 
 	// Read the Person from the XML file
 	file, err = os.Open("person.xml")
