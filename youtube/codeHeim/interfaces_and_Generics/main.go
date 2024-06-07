1ab33c4509bab715553a02001b250f68291d40f8
widefoot1974 <widefoot1974@naver.com>
2024-06-07 09:42:42 +0900

    .

--- youtube/codeHeim/interfaces_and_Generics/main.go
+++ youtube/codeHeim/interfaces_and_Generics/main.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
+// Sortable is a constraint that requires the type
+// to support comparison operators
+type Sortable interface {
+	~int | ~float64 | ~string
+}
+
+// Generic function to sort a slice of any Sortable type
+func GenericSort[T Sortable](slice []T) {
+	for i := 0; i < len(slice); i++ {
+		for j := i + 1; j < len(slice); j++ {
+			if slice[i] > slice[j] {
+				slice[i], slice[j] = slice[j], slice[i]
+			}
+		}
+	}
+}
+
 func main() {
-	fmt.Println("vim-go")
+	intSlice := []int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5}
+	stringSlice := []string{
+		"apple", "orange",
+		"banana", "pear",
+	}
+
+	GenericSort(intSlice)
+	GenericSort(stringSlice)
+
+	fmt.Println("Sorted intSliace:", intSlice)
+	fmt.Println("Sorted stringSlice:", stringSlice)
 }
