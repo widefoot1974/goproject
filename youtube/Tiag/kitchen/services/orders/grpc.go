548c24dbc2472fc774774586d692fdde6bb35b26
widefoot1974 <widefoot1974@naver.com>
2024-06-09 22:30:45 +0900

    add youtube/Tiag

---
+++ youtube/Tiag/kitchen/services/orders/grpc.go
@@ -0,0 +1,13 @@
+package main
+
+type gRPCServer struct {
+	addr string
+}
+
+func NewGRPCServer(addr string) *gRPCServer {
+	return &gRPCServer{addr: addr}
+}
+
+func (s *gRPCServer) Run() error {
+
+}
