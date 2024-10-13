104dda42cdb1dc2320c5579ec7b61a3c16c8dd75
widefoot1974 <widefoot1974@naver.com>
2024-10-13 11:08:17 +0900

    2024/10/13

---
+++ go-skcho/log_zap3/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"time"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
+	url := "https://chatgpt.com/c/66f8b326-21f8-800e-9f89-1a521ec922a9"
+	logger.Info("failed to fetch URL",
+		// Structured context as strongly typed Field values.
+		zap.String("url", url),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+}
